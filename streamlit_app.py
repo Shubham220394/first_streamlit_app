6192de55fadf5b9cffedeecbe677e8448ff8e7d1
Shubham Upadhye <127383670+Shubham220394@users.noreply.github.com>
2023-03-09 13:15:07 +0530

    Update streamlit_app.py
    Added Icons

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & blueberry oatmeal')
-streamlit.text('kale, Spinach and rocket smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('Breakfast favorites')
+streamlit.text('🥣Omega 3 & blueberry oatmeal')
+streamlit.text('🥗kale, Spinach and rocket smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
